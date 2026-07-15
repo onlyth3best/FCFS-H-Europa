d6b8404e0b87048184239dbe5f20ec4dfc7b00c7
01ghostboy10 <levana.rai.simcik@mensa.cz>
2026-07-16 00:08:43 +0200

    basic start scrn

---
+++ * SCRIPTS/scroller.gd
@@ -0,0 +1,46 @@
+extends Control
+
+@export var speed := 77.0
+
+@onready var label_a := $Label1
+@onready var label_b := $Label2
+
+var label_height := 0.0
+
+func _ready():
+	label_height = label_a.size.x
+	label_b.position.x = label_a.position.x + label_height
+
+func _process(delta):
+	label_a.position.x += speed * delta
+	label_b.position.x += speed * delta
+
+	if label_a.position.x >= label_height:
+		label_a.position.x = label_b.position.y - label_height
+
+	if label_b.position.x >= label_height:
+		label_b.position.x = label_a.position.y - label_height
+#
+#
+#extends Control
+#
+#@export var speed := 27.0
+#
+#@onready var label_a := $Label1
+#@onready var label_b := $Label2
+#
+#var label_height := 0.0
+#
+#func _ready():
+	#label_height = label_a.size.x
+	#label_b.position.x = label_a.position.x + label_height
+#
+#func _process(delta):
+	#label_a.position.x -= speed * delta
+	#label_b.position.x -= speed * delta
+#
+	#if label_a.position.x <= -label_height:
+		#label_a.position.x = label_b.position.y + label_height
+#
+	#if label_b.position.x <= -label_height:
+		#label_b.position.x = label_a.position.y + label_height
